58db786e1f6a3338f35ccadfe287b58bb8591bc3
arrow767 <i.silnov.anton@gmail.com>
2020-07-05 21:18:08 +0400

    Commit2

--- random/main.js
+++ random/main.js
@@ -1,16 +1,20 @@
 let arr = [];
 let calc = {};
 
+
+//генерируем рнадомное число
 function genRandom(max, min) {
   return Math.floor(Math.random() * (max - min + 1));
 }
 
+//генерируем массив рандомных чисел
 function genArray(array, length) {
   for (i = 0; i < length; i++) {
     array.push(genRandom(10, 1));
   }
 }
 
+//подсчитываем количество эллементов в массиве
 function calcRes(array) {
   for (i = 0; i < arr.length; i++) {
     calc[array[i]] ? calc[array[i]]++ : (calc[array[i]] = 1);
