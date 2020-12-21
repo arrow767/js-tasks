bf18a495eb78755d03b76048ce5a9e789f16feeb
arrow767 <i.silnov.anton@gmail.com>
2020-12-21 22:55:58 +0400

    up)to_upperCase

--- two_sum/index.js
+++ two_sum/index.js
@@ -1,26 +1,4 @@
-const toUpperCaseFirstLatter = (word) => {
-    console.log(word[0].toUpperCase() + (word.slice(1)))
-}
 
-toUpperCaseFirstLatter('любое слово')
-
-let arr = ['ass']
-
-//Задача twoSum https://leetcode.com/problems/two-sum/
-
-let nums = [2,5,5,11]
-let target = 10
-let ourSum = 0
-
-const twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        for(let k = i+1; k < nums.length; k++){
-            if(nums[i]+nums[k] === target){
-                return [i, k]
-            }
-        }
-    }
-};
 
 
 // const twoSum = function(nums, target) {
